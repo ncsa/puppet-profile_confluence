1cb9b14f669f90764217d8810cf0b687f6007324
Andy Loftus <aloftus@gmail.com>
2022-09-22 19:05:19 +0000

    array of cron file names

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,10 +5,27 @@
 class profile_confluence {
 # Recursively copy all backup files
 
-  file { '/root/cron_scripts/':
-    ensure  => directory,
-    source  => "puppet:///modules/${module_name}/root/cron_scripts/",
-    recurse => true,
+
+  $config_files = [
+    '/root/cron_scripts/confluence-backup.conf',
+    '/root/cron_scripts/confluence-db.conf',
+  ]
+
+  $cron_files = [ '/root/cron_scripts/wiki-backup.sh' ]
+
+  file {
+    $config_files:
+      mode => '0440',
+      ;
+    $cron_files:
+      mode => '0660',
+      ;
+    defaults:
+      ensure => file,
+      user  => root,
+      group => root,
+      source  => "puppet:///modules/${module_name}${name}",
+      ;
   }
 
   cron { 'confluence_backup':
@@ -18,3 +35,5 @@ class profile_confluence {
     environment => ['SHELL=/bin/sh', 'MAILTO=meberger@illinois.edu'],
   }
 }
+
+
