c70a929d1ec9adb887c509a7fb7fa7b93ef1964f
Andy Loftus <aloftus@gmail.com>
2024-03-16 18:57:46 +0000

    update locale, remove encoding specification

--- manifests/init.pp
+++ manifests/init.pp
@@ -48,8 +48,8 @@ class profile_confluence (
 
   postgresql::server::database { $db_name :
     comment  => 'Confluence',
-    encoding => 'utf8',
-    locale   => 'utf8',
+    locale   => 'en_US.UTF-8',
+    #encoding => 'UTF8',
   }
 
   $pwdhash = postgresql::postgresql_password( $db_user, $db_password )
